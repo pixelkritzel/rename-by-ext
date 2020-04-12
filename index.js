560159a6dbabe28cab27cfb948a7b5e4ba727553
Timo Zoeller <timo@pixelkritzel.de>
2020-04-12 13:25:34 +0200

    Fixes node engines > v6

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ if (process.argv.length < 7 && !process.argv.includes('--help')) {
 
 if (process.argv.includes('--help')) {
   console.log(`
-rename-by-ext - Rename files by their extension 
+rename-by-ext - Rename files by their extension
 
 The idea is to rename two lists of files by their extension. For an example movie files and their subtitles.
 It's necessary for them to be in the same order.
@@ -19,11 +19,12 @@ rename-by-extension PATH --lead LEAD_EXTENSION --follow FOLLOW_EXTENSION
 
 
 `);
-process.exit();
+  process.exit();
 }
 
 const pathParameter = process.argv[2] ? process.argv[2] : '';
-const absolutePath = pathParameter[0] === '/' ? pathParameter : path.normalize(process.env.PWD + '/' + pathParameter );
+const absolutePath =
+  pathParameter[0] === '/' ? pathParameter : path.normalize(process.env.PWD + '/' + pathParameter);
 
 const leadIndex = process.argv.indexOf('--lead') + 1;
 const leadExtension = process.argv[leadIndex];
@@ -34,12 +35,18 @@ const followExtension = process.argv[followIndex];
 const filenames = fs.readdirSync(absolutePath);
 
 const leads = filenames.filter(name => !name.startsWith('.') && name.endsWith(`.${leadExtension}`));
-const follows = filenames.filter(name => !name.startsWith('.') && name.endsWith(`.${followExtension}`));
+const follows = filenames.filter(
+  name => !name.startsWith('.') && name.endsWith(`.${followExtension}`)
+);
 
 if (leads.length == follows.length) {
-  for(let i = 0; i < leads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     let leadName = leads[i].split(`.${leadExtension}`)[0];
     let follower = follows[i];
-    fs.rename(`${absolutePath}/${follower}`, `${absolutePath}/${leadName}.${followExtension}`);
+    fs.rename(
+      `${absolutePath}/${follower}`,
+      `${absolutePath}/${leadName}.${followExtension}`,
+      () => undefined
+    );
   }
 }
